8a748f3f6e7277eae82e2f8bcb27ea1da3104bba
Lucas Wantz da Motta <lucaswantz@hotmail.com>
2020-03-26 20:44:38 -0300

    Antes de inciar o as rotas

--- omnistack/aulas/backend/src/index.js
+++ omnistack/aulas/backend/src/index.js
@@ -1,10 +1,9 @@
-// Importar as funcionalidades do expres
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-// Mandar ouvir a porta 3333
 app.listen(3333);
--- omnistack/aulas/backend/src/routes.js
+++ omnistack/aulas/backend/src/routes.js
@@ -1,17 +1,15 @@
-const express = require('express');
-
+const express = require("express");
 const routes = express.Router();
 
-
 // Rota raiz, quando chama sem barra
-routes.post('/users', (request, response) => {
+routes.post("/users", (request, response) => {
   const body = request.body;
-  
+
   console.log(body);
 
   return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Lucas Wantz da Motta'
+    evento: "Semana OmniStack 11.0",
+    aluno: "Lucas Wantz da Motta"
   });
 });
 
